2b1259ad651e17962a812b83393f315e5247166a
Tyler Sanbar <tylersanbar@gmail.com>
2020-12-10 08:58:23 -0600

    test

--- project4/myfs_lib_support.h
+++ project4/myfs_lib_support.h
@@ -14,7 +14,7 @@ void myfs_init_directory_block_and_index_node(INDEX_NODE *inode, BLOCK *block,
 				    INDEX_NODE_REFERENCE self_inode_reference,
 				    INDEX_NODE_REFERENCE parent_inode_reference);
 
-INDEX_NODE_REFERENCE myfs_find_entity_in_directory(INDEX_NODE* index_node, char* element_name);
+//INDEX_NODE_REFERENCE myfs_find_entity_in_directory(INDEX_NODE* index_node, char* element_name);
 INDEX_NODE_REFERENCE myfs_find_entity_in_directory(INDEX_NODE *index_node, char *element_name,
 						   BLOCK *block, BLOCK_REFERENCE *ref, int *entry_index);
 int myfs_path_to_index_node(char *cwd, char * path, INDEX_NODE_REFERENCE *parent,
@@ -33,7 +33,7 @@ int myfs_find_directory_hole(BLOCK_REFERENCE head_directory_block_ref,
 			     BLOCK_REFERENCE *directory_block_ref,
 			     int *directory_index);
 
-INDEX_NODE_REFERENCE myfs_find_index_node_hole();
+INDEX_NODE_REFERENCE myfs_find_index_node_hole(BLOCK* block, BLOCK_REFERENCE* bRef);
 int myfs_find_open_bit(unsigned char value);
 BLOCK_REFERENCE myfs_allocate_new_block(BLOCK *volume_block);
 int myfs_remove_directory_entry(BLOCK_REFERENCE dir_ref, char *name);
@@ -43,8 +43,6 @@ void myfs_clear_all_index_node_entries(BLOCK *index_node_block);
 int myfs_append_new_block_to_existing_block(BLOCK *volume_block,
 					    BLOCK *block,
 					    BLOCK_REFERENCE *block_ref);
-void myfs_clear_all_index_node_entries(BLOCK *index_node_block);
-
 // Project 4
 INDEX_NODE_REFERENCE myfs_create_file(BLOCK *volume_block,
 				      INDEX_NODE_REFERENCE parent, char *name);
