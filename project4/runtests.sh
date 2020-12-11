b24516029524153322d738b756004a433c932b1d
Tyler Sanbar <tylersanbar@gmail.com>
2020-12-10 21:28:44 -0600

    t

--- project4/runtests.sh
+++ project4/runtests.sh
@@ -4,15 +4,24 @@
 
 echo Running Tests
 
-./Tests/test1 > t1.out
-./Tests/test2 > t2.out
-./Tests/test3 > t3.out
-./Tests/test4 > t4.out
+./Tests/project4_test1.sh > t1.out
+./Tests/project4_test2.sh > t2.out
+./Tests/project4_test3.sh > t3.out
+./Tests/project4_test4.sh > t4.out
+./Tests/project4_test5.sh > t5.out
+./Tests/project4_test6.sh > t6.out
+./Tests/project4_test7.sh > t7.out
+./Tests/project4_test8.sh > t8.out
 
 echo Finished Running Tests
 
 echo Making diff files
-diff Tests/test1.out t1.out > t1diff
-diff Tests/test2.out t2.out > t2diff
-diff Tests/test3.out t3.out > t3diff
-diff Tests/test4.out t4.out > t4diff
+diff Tests/project4_test1.out t1.out> t1diff
+diff Tests/project4_test2.out t2.out > t2diff
+diff Tests/project4_test3.out t3.out > t3diff
+diff Tests/project4_test4.out t4.out > t4diff
+diff Tests/project4_test5.out t5.out > t1diff
+diff Tests/project4_test6.out t6.out > t2diff
+diff Tests/project4_test7.out t7.out > t3diff
+diff Tests/project4_test8.out t8.out > t4diff
+
